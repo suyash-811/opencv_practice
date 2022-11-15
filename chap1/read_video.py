a0425deedbe23374b19466b48869ebede21dde86
Suyash Sonawane <sonawanesuyash@gmail.com>
2022-11-15 22:00:38 +0100

    Finished chapter 1

--- chap1/read_video.py
+++ chap1/read_video.py
@@ -2,7 +2,15 @@ import cv2 as cv
 from pathlib import Path
 
 image_folder = Path(__file__).parents[1] / "data"
-cap = cv.VideoCapture(str(image_folder/"tree.avi"))
+# cap = cv.VideoCapture(str(image_folder/"tree.avi"))
+cap = cv.VideoCapture(0)
+cap.set(3, 640) # width
+cap.set(4, 480) # height
+cap.set(10, 100) # brightness
+# cehck the docs to see the other ids/variables
 
 while True:
     exists, frame = cap.read()
+    cv.imshow("Video", frame)
+    if cv.waitKey(10) & 0xFF==ord('q'):
+        break
